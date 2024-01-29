f32bf61c0f7a7e25ad356d99224b1530dad826f4
moradilahroodi <ssn.moradi@gmail.com>
2024-01-29 11:01:00 +0330

    desktop complete

--- script.js
+++ script.js
@@ -69,6 +69,28 @@ $(function () {
     $("#counter").appear();
 
     // counter
+
+    // panels
+    const panels = document.querySelectorAll('.panel')
+
+    function toggleOpen() {
+        if (this.classList.contains('open')) {
+            this.classList.remove('open');
+        } else {
+            panels.forEach(panel => panel.classList.remove('open'));
+            this.classList.toggle('open');
+        }
+    }
+
+    function toggleActive(e) {
+        if (e.propertyName.includes('flex'))
+            this.classList.toggle('open-active')
+    }
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
+    // panels
+
 });
 
 
